75207b155b1d91b2411d1931d047d92202d2af54
Zxuuuu <1521786784@qq.com>
2025-01-19 22:01:44 +0800

    update

--- SNEusage.py
+++ SNEusage.py
@@ -1,34 +1,77 @@
-
 from matplotlib import pyplot as plt
 import pandas as pd
 import SNE
 
-# 读取三个CSV文件
+# 读取十个CSV文件
 df1 = pd.read_csv('train_result/output_features/gearbox_anomaly.csv', header=None)
 df2 = pd.read_csv('train_result/output_features/gearbox_arch.csv', header=None)
 df3 = pd.read_csv('train_result/output_features/gearbox_health.csv', header=None)
+d3 = pd.read_csv('train_result/output_features/gearbox_health3.csv', header=None)
+d4 = pd.read_csv('train_result/output_features/gearbox_health4.csv', header=None)
+d5 = pd.read_csv('train_result/output_features/gearbox_health5.csv', header=None)
+d6 = pd.read_csv('train_result/output_features/gearbox_health6.csv', header=None)
+d7 = pd.read_csv('train_result/output_features/gearbox_health7.csv', header=None)
+d8 = pd.read_csv('train_result/output_features/gearbox_health8.csv', header=None)
+d9 = pd.read_csv('train_result/output_features/gearbox_health9.csv', header=None)
 
 # 将DataFrame转换为numpy数组
 X1 = df1.values
 X2 = df2.values
 X3 = df3.values
+X4 = d3.values
+X5 = d4.values
+X6 = d5.values
+X7 = d6.values
+X8 = d7.values
+X9 = d8.values
+X10 = d9.values
 
 # 进行t-SNE降维
-X_norm1 = SNE.sen_huatu(X1, 30)
-X_norm2 = SNE.sen_huatu(X2, 30)
-X_norm3 = SNE.sen_huatu(X3, 30)
+X_norm1 = SNE.sen_huatu(X1, 10)
+X_norm2 = SNE.sen_huatu(X2, 10)
+X_norm3 = SNE.sen_huatu(X3, 10)
+X_norm4 = SNE.sen_huatu(X4, 10)
+X_norm5 = SNE.sen_huatu(X5, 10)
+X_norm6 = SNE.sen_huatu(X6, 10)
+X_norm7 = SNE.sen_huatu(X7, 10)
+X_norm8 = SNE.sen_huatu(X8, 10)
+X_norm9 = SNE.sen_huatu(X9, 10)
+X_norm10 = SNE.sen_huatu(X10, 10)
+
+# 创建一个空的DataFrame来存储所有降维后的数据
+all_data = pd.DataFrame()
+
+# 将每个数据集的降维结果添加到DataFrame中，并添加一个列来区分不同的数据集
+all_data = all_data._append(pd.DataFrame({'Dimension 1': X_norm1[:, 0], 'Dimension 2': X_norm1[:, 1], 'Dataset': 'Anomaly'}))
+all_data = all_data._append(pd.DataFrame({'Dimension 1': X_norm2[:, 0], 'Dimension 2': X_norm2[:, 1], 'Dataset': 'Arch'}))
+all_data = all_data._append(pd.DataFrame({'Dimension 1': X_norm3[:, 0], 'Dimension 2': X_norm3[:, 1], 'Dataset': 'Health'}))
+all_data = all_data._append(pd.DataFrame({'Dimension 1': X_norm4[:, 0], 'Dimension 2': X_norm4[:, 1], 'Dataset': 'H3'}))
+all_data = all_data._append(pd.DataFrame({'Dimension 1': X_norm5[:, 0], 'Dimension 2': X_norm5[:, 1], 'Dataset': 'H4'}))
+all_data = all_data._append(pd.DataFrame({'Dimension 1': X_norm6[:, 0], 'Dimension 2': X_norm6[:, 1], 'Dataset': 'H5'}))
+all_data = all_data._append(pd.DataFrame({'Dimension 1': X_norm7[:, 0], 'Dimension 2': X_norm7[:, 1], 'Dataset': 'H6'}))
+all_data = all_data._append(pd.DataFrame({'Dimension 1': X_norm8[:, 0], 'Dimension 2': X_norm8[:, 1], 'Dataset': 'H7'}))
+all_data = all_data._append(pd.DataFrame({'Dimension 1': X_norm9[:, 0], 'Dimension 2': X_norm9[:, 1], 'Dataset': 'H8'}))
+all_data = all_data._append(pd.DataFrame({'Dimension 1': X_norm10[:, 0], 'Dimension 2': X_norm10[:, 1], 'Dataset': 'H9'}))
+
+# 将所有数据保存到一个CSV文件
+all_data.to_csv('train_result/output_features/all_norm_data.csv', index=False)
 
 # 创建二维图形
 plt.figure(figsize=(10, 6))
 
-# 绘制第一个数据集的散点图
-plt.scatter(X_norm1[:, 0], X_norm1[:, 1], c='red', label='Data1')
+# 绘制第一个数据集的散点图（anomaly，红色）
+plt.scatter(X_norm1[:, 0], X_norm1[:, 1], c='red', label='Anomaly')
 
-# 绘制第二个数据集的散点图
-plt.scatter(X_norm2[:, 0], X_norm2[:, 1], c='green', label='Data2')
-
-# 绘制第三个数据集的散点图
-plt.scatter(X_norm3[:, 0], X_norm3[:, 1], c='blue', label='Data3')
+# 绘制其他数据集的散点图，颜色自定义
+plt.scatter(X_norm2[:, 0], X_norm2[:, 1], c='green', label='Arch')
+plt.scatter(X_norm3[:, 0], X_norm3[:, 1], c='blue', label='Health')
+plt.scatter(X_norm4[:, 0], X_norm4[:, 1], c='orange', label='H3')
+plt.scatter(X_norm5[:, 0], X_norm5[:, 1], c='purple', label='H4')
+plt.scatter(X_norm6[:, 0], X_norm6[:, 1], c='brown', label='H5')
+plt.scatter(X_norm7[:, 0], X_norm7[:, 1], c='pink', label='H6')
+plt.scatter(X_norm8[:, 0], X_norm8[:, 1], c='gray', label='H7')
+plt.scatter(X_norm9[:, 0], X_norm9[:, 1], c='olive', label='H8')
+plt.scatter(X_norm10[:, 0], X_norm10[:, 1], c='cyan', label='H9')
 
 # 添加图例
 plt.legend()
@@ -40,50 +83,3 @@ plt.ylabel('Dimension 2')
 
 # 显示图形
 plt.show()
-
-# from matplotlib import pyplot as plt
-# from mpl_toolkits.mplot3d import Axes3D
-# import pandas as pd
-# import SNE
-#
-# # 读取三个CSV文件
-# df1 = pd.read_csv('train_result/output_features/gearbox_anomaly.csv', header=None)
-# df2 = pd.read_csv('train_result/output_features/gearbox_arch.csv', header=None)
-# df3 = pd.read_csv('train_result/output_features/gearbox_health.csv', header=None)
-#
-# # 将DataFrame转换为numpy数组
-# X1 = df1.values
-# X2 = df2.values
-# X3 = df3.values
-#
-#
-#
-# # 进行t-SNE降维
-# X_norm1 = SNE.sen_huatu(X1, 30)
-# X_norm2 = SNE.sen_huatu(X2, 30)
-# X_norm3 = SNE.sen_huatu(X3, 30)
-#
-# # 创建三维图形
-# fig = plt.figure(figsize=(10, 8))
-# ax = fig.add_subplot(111, projection='3d')
-#
-# # 绘制第一个数据集的散点图
-# ax.scatter(X_norm1[:, 0], X_norm1[:, 1], X_norm1[:, 2], c='red', label='Data1')
-#
-# # 绘制第二个数据集的散点图
-# ax.scatter(X_norm2[:, 0], X_norm2[:, 1], X_norm2[:, 2], c='green', label='Data2')
-#
-# # 绘制第三个数据集的散点图
-# ax.scatter(X_norm3[:, 0], X_norm3[:, 1], X_norm3[:, 2], c='blue', label='Data3')
-#
-# # 添加图例
-# ax.legend()
-#
-# # 添加标题和坐标轴标签
-# ax.set_title('t-SNE 3D Visualization')
-# ax.set_xlabel('Dimension 1')
-# ax.set_ylabel('Dimension 2')
-# ax.set_zlabel('Dimension 3')
-#
-# # 显示图形
-# plt.show()
--- gearbox_SNE_test.py
+++ gearbox_SNE_test.py
@@ -73,8 +73,6 @@ def weights_init(m):
         m.bias.data = torch.ones(m.bias.data.size())
 
 
-
-
 def main():
     feature_encoder = CNNEncoder1.rsnet()
     fc = FullyConnectedLayer()
@@ -88,23 +86,23 @@ def main():
                 SAMPLE_NUM_PER_CLASS) + "shot.pkl")))
         print("load gearbox feature encoder success")
     print("init data folders")
-
-    # 优化器的定义
-    # feature_encoder_optim = torch.optim.Adam(feature_encoder.parameters(), lr=LEARNING_RATE)
-    # feature_encoder_scheduler = StepLR(feature_encoder_optim, step_size=100000, gamma=0.5)
     fc_optim = torch.optim.Adam(fc.parameters(), lr=LEARNING_RATE)
     fc_scheduler = StepLR(fc_optim, step_size=100000, gamma=0.5)
 
-
-
     # TODO(1.19):add a api to get features
-
     # Step 3: build graph
     print("Training...")
     ticks = 1
-    arch128_all=[]
-    health128_all=[]
-    anomaly128_all=[]
+    arch128_all = []
+    health128_all = []
+    anomaly128_all = []
+    h3all=[]
+    h4all=[]
+    h5all=[]
+    h6all=[]
+    h7all=[]
+    h8all=[]
+    h9all=[]
     last_accuracy = 0
     for episode in range(EPISODE):
         # print('episode', episode)
@@ -112,19 +110,17 @@ def main():
         fc_scheduler.step(episode)
         degrees = random.choice([0, 90, 180, 270])
         #########################################################
-        # triplet_num=random.randint(1,9)
         triplet_num=1
-        # health_character_folders_1 = [f'../CWT-1000/gearbox/train/health/WC{triplet_num}',
-        #                               '../CWT-1000/gearbox/train/health']
+        '''wc1'''
         health_character_folders_1 = [f'../CWT3-1000/gearbox/train/health/WC{triplet_num}',
-                                      '../CWT3-1000/gearbox/arch/health']
-        # arch_character_folders_1 = [f'../CWT-1000/gearbox/train/health/WC{triplet_num}',
-        #                             '../CWT-1000/gearbox/train/health']
-        arch_character_folders_1 = [f'../CWT3-1000/gearbox/train/health/WC{triplet_num}',
-                                    '../CWT3-1000/gearbox/arch/health']
-        random_folder = random.choice([f.path for f in os.scandir('../CWT3-1000/gearbox/arch/anomaly') if f.is_dir()])
+                                      '../CWT3-1000/gearbox/arch/health1']
+        '''wc2'''
+        arch_character_folders_1 = [f'../CWT3-1000/gearbox/train/health/WC{triplet_num+1}',
+                                    '../CWT3-1000/gearbox/arch/health2']
+        '''fault'''
+        random_folder = random.choice([f.path for f in os.scandir('../CWT3-1000/gearbox/test/G4/anomaly/WC1') if f.is_dir()])
         anomaly_character_folders_1 = [random_folder,
-                                       '../CWT3-1000/gearbox/arch/anomaly']
+                                       '../CWT3-1000/gearbox/test/G4/anomaly/WC1']
         task_health = tg.OmniglotTask(health_character_folders_1, CLASS_NUM, SAMPLE_NUM_PER_CLASS, BATCH_NUM_PER_CLASS)
         batch_dataloader_health = tg.get_data_loader(task_health, num_per_class=BATCH_NUM_PER_CLASS, split="test",
                                                      shuffle=True, rotation=degrees)
@@ -147,37 +143,159 @@ def main():
         batch_dataloader_anomaly = iter(batch_dataloader_anomaly)
         batches_anomaly, batch_labels_anomaly = next(batch_dataloader_anomaly)
         batch_features_anomaly = feature_encoder(Variable(batches_anomaly).cuda(GPU))
+        ''''''
+        health3_character_folders_1 = [f'../CWT3-1000/gearbox/train/health/WC3',
+                                    '../CWT3-1000/gearbox/arch/health3']
+        task_h3 = tg.OmniglotTask(health3_character_folders_1, CLASS_NUM, SAMPLE_NUM_PER_CLASS, BATCH_NUM_PER_CLASS)
+        batch_dataloader_h3 = tg.get_data_loader(task_h3, num_per_class=BATCH_NUM_PER_CLASS, split="test",
+                                                   shuffle=True,
+                                                   rotation=degrees)
+        batch_dataloader_h3 = iter(batch_dataloader_h3)
+        batches_h3, batch_labels_h3 = next(batch_dataloader_h3)
+        batch_features_h3 = feature_encoder(Variable(batches_h3).cuda(GPU))
+        batch_features_h3=fc(batch_features_h3)
+        ''''''
+        ''''''
+        health4_character_folders_1 = [f'../CWT3-1000/gearbox/train/health/WC4',
+                                    '../CWT3-1000/gearbox/arch/health4']
+        task_h4 = tg.OmniglotTask(health4_character_folders_1, CLASS_NUM, SAMPLE_NUM_PER_CLASS, BATCH_NUM_PER_CLASS)
+        batch_dataloader_h4 = tg.get_data_loader(task_h4, num_per_class=BATCH_NUM_PER_CLASS, split="test",
+                                                   shuffle=True,
+                                                   rotation=degrees)
+        batch_dataloader_h4 = iter(batch_dataloader_h4)
+        batches_h4, batch_labels_h4 = next(batch_dataloader_h4)
+        batch_features_h4 = feature_encoder(Variable(batches_h4).cuda(GPU))
+        batch_features_h4=fc(batch_features_h4)
+        ''''''
+        ''''''
+        health5_character_folders_1 = [f'../CWT3-1000/gearbox/train/health/WC5',
+                                    '../CWT3-1000/gearbox/arch/health5']
+        task_h5 = tg.OmniglotTask(health5_character_folders_1, CLASS_NUM, SAMPLE_NUM_PER_CLASS, BATCH_NUM_PER_CLASS)
+        batch_dataloader_h5 = tg.get_data_loader(task_h5, num_per_class=BATCH_NUM_PER_CLASS, split="test",
+                                                   shuffle=True,
+                                                   rotation=degrees)
+        batch_dataloader_h5 = iter(batch_dataloader_h5)
+        batches_h5, batch_labels_h5 = next(batch_dataloader_h5)
+        batch_features_h5 = feature_encoder(Variable(batches_h5).cuda(GPU))
+        batch_features_h5=fc(batch_features_h5)
+        ''''''
+        ''''''
+        health6_character_folders_1 = [f'../CWT3-1000/gearbox/train/health/WC6',
+                                    '../CWT3-1000/gearbox/arch/health6']
+        task_h6 = tg.OmniglotTask(health6_character_folders_1, CLASS_NUM, SAMPLE_NUM_PER_CLASS, BATCH_NUM_PER_CLASS)
+        batch_dataloader_h6 = tg.get_data_loader(task_h6, num_per_class=BATCH_NUM_PER_CLASS, split="test",
+                                                   shuffle=True,
+                                                   rotation=degrees)
+        batch_dataloader_h6 = iter(batch_dataloader_h6)
+        batches_h6, batch_labels_h6 = next(batch_dataloader_h6)
+        batch_features_h6 = feature_encoder(Variable(batches_h6).cuda(GPU))
+        batch_features_h6=fc(batch_features_h6)
+        ''''''
+        ''''''
+        health7_character_folders_1 = [f'../CWT3-1000/gearbox/train/health/WC7',
+                                    '../CWT3-1000/gearbox/arch/health7']
+        task_h7 = tg.OmniglotTask(health7_character_folders_1, CLASS_NUM, SAMPLE_NUM_PER_CLASS, BATCH_NUM_PER_CLASS)
+        batch_dataloader_h7 = tg.get_data_loader(task_h7, num_per_class=BATCH_NUM_PER_CLASS, split="test",
+                                                   shuffle=True,
+                                                   rotation=degrees)
+        batch_dataloader_h7 = iter(batch_dataloader_h7)
+        batches_h7, batch_labels_h7 = next(batch_dataloader_h7)
+        batch_features_h7 = feature_encoder(Variable(batches_h7).cuda(GPU))
+        batch_features_h7=fc(batch_features_h7)
+        ''''''
+        ''''''
+        health8_character_folders_1 = [f'../CWT3-1000/gearbox/train/health/WC8',
+                                    '../CWT3-1000/gearbox/arch/health8']
+        task_h8 = tg.OmniglotTask(health8_character_folders_1, CLASS_NUM, SAMPLE_NUM_PER_CLASS, BATCH_NUM_PER_CLASS)
+        batch_dataloader_h8 = tg.get_data_loader(task_h8, num_per_class=BATCH_NUM_PER_CLASS, split="test",
+                                                   shuffle=True,
+                                                   rotation=degrees)
+        batch_dataloader_h8 = iter(batch_dataloader_h8)
+        batches_h8, batch_labels_h8 = next(batch_dataloader_h8)
+        batch_features_h8 = feature_encoder(Variable(batches_h8).cuda(GPU))
+        batch_features_h8=fc(batch_features_h8)
+        ''''''
+        ''''''
+        health9_character_folders_1 = [f'../CWT3-1000/gearbox/train/health/WC9',
+                                    '../CWT3-1000/gearbox/arch/health9']
+        task_h9 = tg.OmniglotTask(health9_character_folders_1, CLASS_NUM, SAMPLE_NUM_PER_CLASS, BATCH_NUM_PER_CLASS)
+        batch_dataloader_h9 = tg.get_data_loader(task_h9, num_per_class=BATCH_NUM_PER_CLASS, split="test",
+                                                   shuffle=True,
+                                                   rotation=degrees)
+        batch_dataloader_h9 = iter(batch_dataloader_h9)
+        batches_h9, batch_labels_h9 = next(batch_dataloader_h9)
+        batch_features_h9 = feature_encoder(Variable(batches_h9).cuda(GPU))
+        batch_features_h9=fc(batch_features_h9)
+        ''''''
+
 
         batch_features_arch = fc(batch_features_arch)
         batch_features_health = fc(batch_features_health)
         batch_features_anomaly = fc(batch_features_anomaly)
         '''change 0.1 to 1'''
-        triloss = TripletLoss(margin=1000)
+        triloss = TripletLoss(margin=0.1)
 
         loss_punish = triloss(batch_features_arch, batch_features_health, batch_features_anomaly)
         """Connect to line 112"""
-        if episode>100:
-            if loss_punish==0 and ticks < 24 and triplet_num==1:
+        if episode > 1000:
+            if loss_punish == 0 and ticks < 24 and triplet_num == 1:
                 arch128 = [x_1.cpu().detach().numpy() for x_1 in batch_features_arch]
-                health128=[x_2.cpu().detach().numpy() for x_2 in batch_features_health]
+                health128 = [x_2.cpu().detach().numpy() for x_2 in batch_features_health]
                 anomaly128 = [x_3.cpu().detach().numpy() for x_3 in batch_features_anomaly]
-                ticks=ticks+1
+                h3=[x3.cpu().detach().numpy() for x3 in batch_features_h3]
+                h4 = [x4.cpu().detach().numpy() for x4 in batch_features_h4]
+                h5 = [x5.cpu().detach().numpy() for x5 in batch_features_h5]
+                h6 = [x6.cpu().detach().numpy() for x6 in batch_features_h6]
+                h7 = [x7.cpu().detach().numpy() for x7 in batch_features_h7]
+                h8 = [x8.cpu().detach().numpy() for x8 in batch_features_h8]
+                h9 = [x9.cpu().detach().numpy() for x9 in batch_features_h9]
+                ticks = ticks + 1
                 arch128_all.extend(arch128)
                 health128_all.extend(health128)
                 anomaly128_all.extend(anomaly128)
-                if ticks==24:
+                h3all.extend(h3)
+                h4all.extend(h4)
+                h5all.extend(h5)
+                h6all.extend(h6)
+                h7all.extend(h7)
+                h8all.extend(h8)
+                h9all.extend(h9)
+                if ticks == 24:
                     arch128_all = np.array(arch128_all)
                     health128_all = np.array(health128_all)
                     anomaly128_all = np.array(anomaly128_all)
-
+                    h3all=np.array(h3all)
+                    h4all = np.array(h4all)
+                    h5all = np.array(h5all)
+                    h6all = np.array(h6all)
+                    h7all = np.array(h7all)
+                    h8all = np.array(h8all)
+                    h9all = np.array(h9all)
                     # 保存数据到CSV文件
                     train_result = 'train_result/'  # 假设这是你的保存路径
-                    np.savetxt(train_result +'output_features/'+ 'gearbox_arch.csv', arch128_all, fmt='%.8f', delimiter=',')
-                    np.savetxt(train_result+'output_features/' + 'gearbox_health.csv', health128_all, fmt='%.8f', delimiter=',')
-                    np.savetxt(train_result+'output_features/' + 'gearbox_anomaly.csv', anomaly128_all, fmt='%.8f', delimiter=',')
+                    np.savetxt(train_result + 'output_features/' + 'gearbox_arch.csv', arch128_all, fmt='%.8f',
+                               delimiter=',')
+                    np.savetxt(train_result + 'output_features/' + 'gearbox_health.csv', health128_all, fmt='%.8f',
+                               delimiter=',')
+                    np.savetxt(train_result + 'output_features/' + 'gearbox_anomaly.csv', anomaly128_all, fmt='%.8f',
+                               delimiter=',')
+                    np.savetxt(train_result + 'output_features/' + 'gearbox_health3.csv', h3all, fmt='%.8f',
+                               delimiter=',')
+                    np.savetxt(train_result + 'output_features/' + 'gearbox_health4.csv', h4all, fmt='%.8f',
+                               delimiter=',')
+                    np.savetxt(train_result + 'output_features/' + 'gearbox_health5.csv', h5all, fmt='%.8f',
+                               delimiter=',')
+                    np.savetxt(train_result + 'output_features/' + 'gearbox_health6.csv', h6all, fmt='%.8f',
+                               delimiter=',')
+                    np.savetxt(train_result + 'output_features/' + 'gearbox_health7.csv', h7all, fmt='%.8f',
+                               delimiter=',')
+                    np.savetxt(train_result + 'output_features/' + 'gearbox_health8.csv', h8all, fmt='%.8f',
+                               delimiter=',')
+                    np.savetxt(train_result + 'output_features/' + 'gearbox_health9.csv', h9all, fmt='%.8f',
+                               delimiter=',')
         #########################################################
 
-        loss=loss_punish
+        loss = loss_punish
         loss.backward()
         # torch.nn.utils.clip_grad_norm(feature_encoder.parameters(), 0.5)
         torch.nn.utils.clip_grad_norm(fc.parameters(), 0.5)
